e2fc1e534790c49a954ec2d25762371f22f76672
Noel Pomaville <ntpomaville@gmail.com>
2020-09-10 16:14:24 -0400

    Add JS functionality to place social media icons at bottom reactively

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,29 @@
 //https://discuss.dev.twitch.tv/t/streamers-live-status-on-website/8412/3
 
+let div = document.getElementById('body');
+let bottomBar = document.getElementById('bottom-bar');
+
+let hasVerticalScrollbar;
+
+hasVerticalScrollbar = div.scrollHeight > div.clientHeight;
+
+if (hasVerticalScrollbar == false) {
+    bottomBar.style.position = 'absolute';
+    bottomBar.style.bottom = '0';
+    bottomBar.style.right = '0';
+}
+
+window.onresize = function myFunction() {
+    noVerticalScrollbar = div.scrollHeight < div.clientHeight;
+    console.log(noVerticalScrollbar);
+    if (noVerticalScrollbar == true) {
+        bottomBar.style.position = 'absolute';
+        bottomBar.style.bottom = '0';
+        bottomBar.style.right = '0';
+    }
+    if (noVerticalScrollbar == false) {
+        bottomBar.style.position = 'static';
+        bottomBar.style.bottom = '0';
+        bottomBar.style.right = '0';
+    }
+}
